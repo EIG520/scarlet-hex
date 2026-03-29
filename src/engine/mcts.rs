0f85d596c83a563bcb67193de5712120f8b1c415
EIG520 <72418797+EIG520@users.noreply.github.com>
2026-03-28 20:15:29 -0500

    add a little mcts

--- src/engine/mcts.rs
+++ src/engine/mcts.rs
@@ -1,10 +1,12 @@
 use itertools::Itertools;
-use std::cell::RefCell;
+use std::{cell::RefCell, intrinsics::expf32};
 use crate::hexo::state::{Coord, State};
 
 struct Node {
     tile: Coord,
     children: Vec<usize>,
+    visits: usize,
+    score: f32
 }
 
 pub struct Searcher {
@@ -23,23 +25,33 @@ impl Searcher {
             // select child (always first one until I write actual code)
             let idx = node.children[0];
             node = self.nodes[idx].get_mut();
-            path.push(0usize);
+            path.push(idx);
         }
 
         // Expand
         // Only search in the vicinity of the last move bc idc
+        let mut eval = 0;
         let tile = node.tile;
         for (x,y) in (-8..=8).cartesian_product(-8..=8) {
             if self.state.is_open((tile.0 + x, tile.1 + y)) {
                 node.children.push(nlen);
-                self.nodes.push( RefCell::new(Node {tile: (tile.0 + x, tile.1 + y), children: vec![] }) );
+                self.state.play((tile.0 + x, tile.1 + y));
+                eval = self.state.eval();
+                self.nodes.push( RefCell::new(Node {tile: (tile.0 + x, tile.1 + y), children: vec![], visits: 1, score: sigmoid(eval as f32) }) );
 
                 break;
             }
         }
 
-        // Goof
-        
-        
+        // Update evaluations
+        for p in path {
+            let node = self.nodes[p].get_mut();
+            node.visits += 1;
+            node.score += sigmoid(eval as f32);
+        }
     }
+}
+
+fn sigmoid(x: f32) -> f32 {
+    return 2 / (1 + expf32(-x)) - 1;
 }
